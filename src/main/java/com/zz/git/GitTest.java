423e88abc8b1aa9c2551337da75d4523da244ea6
Zzgo-biubiu <57145704+Zzgo-biubiu@users.noreply.github.com>
2021-08-11 22:25:52 +0800

    pull commit

--- src/main/java/com/zz/git/GitTest.java
+++ src/main/java/com/zz/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("现在是远程了 test");
     }
 }
